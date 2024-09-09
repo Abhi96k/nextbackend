38a5d353f55cc4c75fc853881ff5aa60b436e093
abhi@96k <abhisheknangare96k@gmail.com>
2024-09-09 16:05:21 +0530

    feat: Add Signup component and Loading component
    - Added Signup component to handle user sign up functionality.
    - Added Loading component to display a loading indicator.

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,13 @@
+export async function GET() {
+  return Response.json({
+    username: "Abhishek",
+    email: "abhisheknangare96k@gmail.com",
+  });
+}
+
+export async function POST() {
+  return Response.json({
+    username: "Abhisheknangare",
+    email: "abhisheknangare96k@gmail.com",
+  });
+}
